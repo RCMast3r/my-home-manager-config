06bad95dc5f617c6de6e48afbbb492392ee0608e
Ben Hall <rcmast3r1@gmail.com>
2023-08-25 19:01:18 -0400

    added in normal vscode keybindings

--- programs.nix
+++ programs.nix
@@ -1,4 +1,4 @@
-{pkgs, ...}:
+{ pkgs, ... }:
 {
   home-manager = {
     enable = true;
@@ -7,7 +7,7 @@
     mutableExtensionsDir = false;
     enable = true;
     # will at some point include these: https://github.com/RCMast3r/vscode_extensions/blob/master/combined_ext_list.sh
-    extensions = [ pkgs.vscode-extensions.twxs.cmake pkgs.vscode-extensions.ms-vscode.cmake-tools pkgs.vscode-extensions.shd101wyy.markdown-preview-enhanced pkgs.vscode-extensions.ms-vscode.cpptools pkgs.vscode-extensions.jnoortheen.nix-ide pkgs.vscode-extensions.vscodevim.vim] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+    extensions = [ pkgs.vscode-extensions.twxs.cmake pkgs.vscode-extensions.ms-vscode.cmake-tools pkgs.vscode-extensions.shd101wyy.markdown-preview-enhanced pkgs.vscode-extensions.ms-vscode.cpptools pkgs.vscode-extensions.jnoortheen.nix-ide pkgs.vscode-extensions.vscodevim.vim ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
       {
         name = "platformio-ide";
         publisher = "platformio";
@@ -18,15 +18,57 @@
         name = "cmake-language-support-vscode";
         publisher = "josetr";
         version = "0.0.9";
-        sha256 ="sha256-LNtXYZ65Lka1lpxeKozK6LB0yaxAjHsfVsCJ8ILX8io=";
+        sha256 = "sha256-LNtXYZ65Lka1lpxeKozK6LB0yaxAjHsfVsCJ8ILX8io=";
       }
     ];
-    
+
     userSettings = {
       "files.userSettings" = "on";
       "files.autoSave" = "afterDelay";
-      "cmake.configureOnOpen"= false;
+      "cmake.configureOnOpen" = false;
+      "editor.minimap.enabled" = false;
     };
+    keybindings = [
+      {
+        key = "ctrl+shift+x";
+        command = "-workbench.view.extensions";
+        when = "viewContainer.workbench.view.extensions.enabled";
+      }
+      {
+        key = "ctrl+shift+x";
+        command = "workbench.action.closeActiveEditor";
+      }
+      {
+        key = "ctrl+w";
+        command = "-workbench.action.closeActiveEditor";
+      }
+      {
+        key = "ctrl+shift+tab";
+        command = "-workbench.action.quickOpenNavigatePreviousInEditorPicker";
+        when = "inEditorsPicker && inQuickOpen";
+      }
+      {
+        key = "ctrl+shift+tab";
+        command = "-workbench.action.quickOpenLeastRecentlyUsedEditorInGroup";
+        when = "!activeEditorGroupEmpty";
+      }
+      {
+        key = "ctrl+shift+tab";
+        command = "workbench.action.previousEditor";
+      }
+      {
+        key = "ctrl+pageup";
+        command = "-workbench.action.previousEditor";
+      }
+      {
+        key = "ctrl+tab";
+        command = "workbench.action.nextEditor";
+      }
+      {
+        key = "ctrl+pagedown";
+        command = "-workbench.action.nextEditor";
+      }
+    ];
   };
   git = {
     enable = true;
